1f9da37f04e7ba6df034b9b7890c4d5493b69cc7
迟谋 <705359976@qq.com>
2020-05-19 20:51:34 +0800

    Initial commit

---
+++ Assets/Scenes/Scripts/Main.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    private S1 s1=new S1();
+
+    void Start()
+    {        
+        Debug.Log(s1.ReverseLeftWords("asdasdaefse", 2));
+    }
+
+   
+}
---
+++ Assets/Scenes/Scripts/S1.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S1 : MonoBehaviour
+{
+    public string ReverseLeftWords(string s, int n)
+    {    
+        return s.Substring(n) + s.Substring(0, n);
+    }
+
+}
