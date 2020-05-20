e914baad609f4fc3fa689f7bbb2b2eb59cdb6acf
迟谋 <705359976@qq.com>
2020-05-20 20:25:35 +0800

    链表题

--- Assets/Scenes/Scripts/S1.cs
+++ Assets/Scenes/Scripts/S1.cs
@@ -2,11 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class S1 : MonoBehaviour
+public class S1
 {
     public string ReverseLeftWords(string s, int n)
     {    
         return s.Substring(n) + s.Substring(0, n);
+    }
+
+}
+public class S2
+{
+
+public class ListNode
+ {
+    public int val;
+    public ListNode next;
+    public ListNode(int x) { val = x; }
+}
+
+    public ListNode GetKthFromEnd(ListNode head, int k)
+    {
+        if (head == null) return null;
+        List<ListNode> listNodes=new List<ListNode>();
+        ListNode temp = head;
+        while (temp!= null)
+        {
+            listNodes.Add(temp);
+            temp = temp.next;
+        }
+        var _index= listNodes.Count-k;
+        if (_index < 0)
+        {
+            return null;
+        }
+        return listNodes[_index];
     }
 
 }
